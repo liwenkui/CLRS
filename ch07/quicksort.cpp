43a2adb43d5229a0c560169f2f3fa626c91d213a
liwenkui <liwenkui0229@icloud.com>
2016-12-29 20:43:54 +0800

    modify

--- ch07/quicksort.cpp
+++ ch07/quicksort.cpp
@@ -1,8 +1,8 @@
 //CLRS QuickSort p.95(C),p.170(E)
 #include <vector>
 #include <iostream>
-#include <iterator>
 #include <algorithm>
+#include <iterator>
 template<typename T>
 T Partition(T begin,T end){
 	auto pivot=*begin;
@@ -26,8 +26,13 @@ void QuickSort(T begin,T end){
 }
 int main(){
 	std::vector<int> ivec{2,8,7,1,3,5,6,4};
+	int arry[]={2,8,7,1,3,5,6,4};
 	QuickSort(ivec.begin(),ivec.end());
+	QuickSort(std::begin(arry),std::end(arry));
 	for(auto c:ivec)
+		std::cout<<c<<' ';
+	std::cout<<std::endl;
+	for(auto c:arry)
 		std::cout<<c<<' ';
 	std::cout<<std::endl;
 }
